e71d528ceebeb55a36de31134150d59b8bf7b9fd
lucax13 <lucas13lima2006@gmail.com>
2025-04-29 12:00:08 -0300

    Create ConexaoBD.php

---
+++ src/Database/ConexaoBD.php
@@ -0,0 +1,35 @@
+<?php
+namespace ExemploCrud;
+
+use Exception;
+use PDO;
+use Throwable;
+
+abstract class ConexaoBD
+{
+    private static PDO $conexao;
+    private static string $servidor = "localhost";
+    private static string $usuario = "root";
+    private static string $senha = "";
+    private static string $banco = "vendas";
+
+    public static function  getConexao():PDO
+    {
+        if(!isset(self::$conexao)){
+            try{
+
+                self::$conexao = new PDO(
+                    "mysql:host=".self::$servidor.";dbname=".self::$banco.";charset=utf8",
+                    self::$usuario, self::$senha  
+                );
+            
+                self::$conexao->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            } catch (Throwable $erro){
+
+                throw new Exception("Erro ao conectar com o banco de dados!");
+            }
+
+        }
+        return self::$conexao;
+    }
+}
