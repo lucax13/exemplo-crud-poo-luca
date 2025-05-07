f39a806c89bb2b9a0a2ac897aaa67dfdb38032e9
lucax13 <lucas13lima2006@gmail.com>
2025-05-07 09:01:12 -0300

    Update Fabricante.php

--- src/Models/Fabricante.php
+++ src/Models/Fabricante.php
@@ -1,15 +1,41 @@
 <?php 
 namespace ExemploCrud;
 
+use InvalidArgumentException;
+
 final class Fabricante {
     private ?int $id;
-    private string $nome
+    private string $nome;
 
     public function __construct(string $nome, ?int $id = null)
     {
         $this->setNome($nome);
         $this->setId($id);
         $this->validar();
-    
     }
-}
+
+    private function validar():void
+    {
+        if(empty($this->nome)){
+            throw new InvalidArgumentException("Nome é obrigatorio");
+        }
+    }
+
+    public function getId(): ?int {
+        return $this->id;
+    }
+
+    public function getNome(): string {
+        return $this->nome;
+    }
+
+    private function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    private function setNome(?int $nome): void
+    {
+        $this->nome = $nome;
+    }
+} 
